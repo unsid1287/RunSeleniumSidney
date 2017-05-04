0de2894fd57a7fd146e97d7827b97e58ffe3b45c
Sid A <Baby@ANDREAs-MacBook-Pro.local>
2017-05-03 20:13:49 -0400

    Develop open Firefox browser

--- src/com/jamstop/TheJam.java
+++ src/com/jamstop/TheJam.java
@@ -1,11 +1,23 @@
 package com.jamstop;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by mkale9 on 4/24/17.
  */
 public class TheJam {
     public static void main(String[] args) {
         System.out.println("hello world");
-        }
+
+        WebDriver dr = new FirefoxDriver();
+
+
     }
+        }
 
